b264da83fdea176295ec3feb4dcb116debb12ad1
Kalididdi Jayarohit Varma <116787916+rohit-kodnest@users.noreply.github.com>
2026-02-12 22:15:27 +0530

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/build',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+        }),
+    ],
+    devServer: {
+        static: './build',
+    },
+};
