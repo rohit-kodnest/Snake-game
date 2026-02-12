ee79b8aa2166b3064f9e2d72b3bfc2ef6a1ec34e
Kalididdi Jayarohit Varma <116787916+rohit-kodnest@users.noreply.github.com>
2026-02-12 22:15:26 +0530

    Add src/index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import SnakeGame from './App';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <SnakeGame />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
